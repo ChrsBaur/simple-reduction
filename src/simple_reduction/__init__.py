994cd20985454c8b87735acad78be0682c1ba541
Christian Baur <christian.baur@alexanderthamm.com>
2023-06-06 19:17:40 +0200

    Initial commit

---
+++ src/simple_reduction/__init__.py
@@ -0,0 +1,9 @@
+"""
+Simple Reduction
+
+Application to do reduction of PDF invoices based on coordinates.
+"""
+__title__ = "simple-reduction"
+__copyright__ = "© 2023 Databaur"
+
+from .version import __version__
